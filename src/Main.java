4b14b73c830a3c74b225716015cbf0c7e3f701c1
duffelmuffinvii <slkbrb@gmail.com>
2022-02-08 15:31:34 -0500

    randomized list of 40 numbers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.util.ArrayList;
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
 
+        Random r = new Random();
+        r.setSeed(10);
+        ArrayList<Double> input = new ArrayList<>();
+
+        for (int i = 0; i < 40; i++) {
+            input.add((20 * r.nextDouble())-10);
+            System.out.println(input.get(i));
+        }
+
+
     }
+
+
 }
